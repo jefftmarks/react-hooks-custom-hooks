2b90c24d9d6ffc1c9fba291cf06aaab0ce2f93bc
Jeff Marks <jeff.t.marks@gmail.com>
2022-08-20 14:55:49 -0400

    done with assignment

--- src/hooks/useQuery.js
+++ src/hooks/useQuery.js
@@ -1,25 +1,20 @@
 import { useEffect, useState } from "react";
 
-// take in the url
 function useQuery(url) {
-  const [isLoaded, setIsLoaded] = useState(false);
-  // rename `posts` to a more generic `data`
-  const [data, setData] = useState(null);
+   const [isLoaded, setIsLoaded] = useState(false);
+   const [data, setData] = useState(null);
+ 
+   useEffect(() => {
+     setIsLoaded(false);
+     fetch("http://localhost:4000/posts")
+       .then((r) => r.json())
+       .then((data) => {
+         setData(data);
+         setIsLoaded(true);
+       });
+   }, []);
 
-  useEffect(() => {
-    setIsLoaded(false);
-    fetch(url)
-      .then((r) => r.json())
-      .then((data) => {
-        setData(data);
-        setIsLoaded(true);
-      });
-  }, [url]);
-  // the url is now a dependency
-  // we want to use the side effect whenever the url changes
-
-  // return an *object* with the data and isLoaded state
-  return { data, isLoaded };
+   return { data, isLoaded }
 }
 
 export default useQuery;
